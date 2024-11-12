1259ba3719724b9e139991211f987fbe6b6162e8
Cole Nugteren <canslp01@gmail.com>
2024-11-12 07:43:00 -0600

    sword

--- scripts/player_hand.gd
+++ scripts/player_hand.gd
@@ -2,12 +2,13 @@ extends Node3D
 
 @export var pf_bomb : PackedScene
 @onready var item_bin = get_parent().get_parent().get_parent().get_node("item bin")
+var throw_vector = Vector3(1,1,0)
 
 func throw_bomb():
 	var bomb = pf_bomb.instantiate()
 	item_bin.add_child(bomb)
 	bomb.global_position = global_position
-	bomb.linear_velocity = -global_basis.z*10
+	bomb.linear_velocity = global_basis*throw_vector*10
 
 func _input(event):
 	if event.is_action_pressed("click"):
