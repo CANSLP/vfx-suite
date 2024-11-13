ae3f2b4da6fae36b12498431e4e82315cd5d27c3
Cole Nugteren <canslp01@gmail.com>
2024-11-13 14:15:00 -0600

    bomb progress

--- scripts/bomb_explosion.gd
+++ scripts/bomb_explosion.gd
@@ -2,14 +2,24 @@ extends Node3D
 
 var time = 0.0
 var mat_rims : Material
+var mat_blast : Material
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	time = 0
+	
 	mat_rims = $rims.material_override
 	mat_rims = mat_rims.duplicate()
 	$rims.material_override = mat_rims
 	$rims.scale = Vector3(0,0,0)
+	
+	mat_blast = $blast.material_override
+	mat_blast = mat_blast.duplicate()
+	$blast.material_override = mat_blast
+	$blast.scale = Vector3(0,0,0)
+	$blast.global_rotation = Vector3(0,0,0)
+	
+	$smoke.emitting = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,6 +29,11 @@ func _process(delta):
 	
 	$rims.scale = Vector3(1,1,1)*clamp(pow(time*5.0,0.125),0,1)
 	mat_rims.set("shader_parameter/time",clamp(pow(time*5.0,0.125),0,1));
+	mat_rims.set("shader_parameter/brightness",10.0*clamp(pow(time*5.0,0.125),0,1));
 	
-	if time > 5.0:
+	$blast.scale = Vector3(1,1,1)*clamp(pow(time*3.0,0.125),0,1)*2.5
+	$blast.global_rotation = Vector3(0,0,0)
+	mat_blast.set("shader_parameter/time",clamp(pow(time*3.0,0.125),0,1));
+	
+	if time > 2.5:
 		queue_free()
